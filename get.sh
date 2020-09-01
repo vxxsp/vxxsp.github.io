985432cdfed39c64683363fdef970188e17f524a
hafiyiman <45912966+hafiyiman@users.noreply.github.com>
2020-09-01 16:19:15 +0800

    removed LFS

---
+++ get.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+root="root"
+echo "<ul>"
+for file in "$root"/*/*; do
+  parentpath="${file#*/}"
+  parent="${parentpath%/*}"
+  filename="${file##*/}"
+  if [[ -z $oldparent ]]; then
+    echo "  <li> $parent </li>" && oldparent="$parent"
+    echo "  <ul>"
+  elif [[ $oldparent != $parent ]]; then
+    echo "  </ul>"
+    echo "  <li> $parent </li>" && oldparent="$parent"
+    echo "  <ul>"
+  fi
+  echo "    <li><a href=\"$parentpath\">$filename</a></li>"
+done
+echo "  </ul>"
+echo "</ul>" 
